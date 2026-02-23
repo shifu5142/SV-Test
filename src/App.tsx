d834e196ff7216368a2cb5765cafa0c71a4f6ea2
SHIFU <tomer5142200@walla.co.il>
2026-02-23 08:22:28 +0200

    fix vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AddFormRoom from './pages/AddFormRoom'
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   // const res = await fetch("http://localhost:3000")חיבור לסרבר
   return (
     <>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
-          <Route path="/AddFormRoom" element={<AddFormRoom />} />
+          <Route path="/" element={</>} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <h1>Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
