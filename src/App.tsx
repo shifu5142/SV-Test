a3f1d66ce038b5c692e16746a8100a4d55f5c9b4
SHIFU <tomer5142200@walla.co.il>
2026-02-26 18:04:31 +0200

    latest clean project update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 function App() {
   // const res = await fetch("http://localhost:3000")חיבור לסרבר
   return (
     <>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={</>} />
-        </Routes>
-      </BrowserRouter> */}
-      <h1>Hello World</h1>
+      <h1>hello</h1>
     </>
   );
 }
