ea080f60d6b52d58a29385d8bdd8f85e2c71d861
SHIFU <tomer5142200@walla.co.il>
2026-02-27 23:50:54 +0200

    last update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   const res = fetch(import.meta.env.VITE_BACKEND_CONNECTION + "/")
     .then((res) => res.json())
     .then((data) => console.log(data));
+  console.log(res);
   return (
     <>
       <h1>hello</h1>
