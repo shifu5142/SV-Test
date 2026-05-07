aba7d1d714721be0c95384230d144380b9479ae8
SHIFU <tomer5142200@walla.co.il>
2026-05-07 15:13:19 +0300

    add request of getting all the exist movies from the backend

--- src/components/search-movie.tsx
+++ src/components/search-movie.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+
 import MenuBar from "@/components/menu-bar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 
 function SearchMovie() {
+  const [searchValue, setSearchValue] = useState("");
+  const BACKEND_URL = (
+    import.meta.env.VITE_BACKEND_CONNECTION || "http://localhost:3000"
+  ).replace(/\/+$/, "");
+  async () => {
+    useEffect(() => {
+      const fetchMovies = async () => {
+        const response = await fetch(BACKEND_URL + "/search-movie", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      };
+      return fetchMovies();
+    }, [searchValue]);
+  };
+
   return (
     <div className="min-h-screen bg-muted/30 flex">
       <MenuBar />
@@ -13,7 +33,11 @@ function SearchMovie() {
             <CardTitle>Search Movies</CardTitle>
           </CardHeader>
           <CardContent>
-            <Input placeholder="Enter movie name" />
+            <Input
+              placeholder="Enter movie name"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
           </CardContent>
         </Card>
       </main>
